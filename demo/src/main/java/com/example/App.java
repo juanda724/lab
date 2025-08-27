9e0cd4337ef12cdaaf46c67e13d6b7f7d481d9b8
juanpe23 <142370092+JuanLBEnjoyer@users.noreply.github.com>
2025-08-27 08:51:55 -0500

    cambio2

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -7,7 +7,8 @@ package com.example;
 public class App 
 {
     public static void main( String[] args )
-    {
+    { 
+        System.out.println( "gayvid love dicks" );
         System.out.println( "ilovecp" );
     }
 }
